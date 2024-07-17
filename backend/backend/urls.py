dc7c061ef817d77235630b2102604c5ac68fb3f0
wgeddes05 <wgeddes05@gmail.com>
2024-07-17 14:51:43 -0400

    modified urls.py, models.py, views.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import DefaultRouter
+from ..booking.views import BookingViewSet
+
+
+router = DefaultRouter()
+router.register(r"bookings", BookingViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
--- backend/booking/models.py
+++ backend/booking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} - {self.email} - {self.date}"
